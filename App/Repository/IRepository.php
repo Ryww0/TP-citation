58e58b499a5e8c95ea73eae66dd78c0aa503cb04
stomruk <s.tomruk35@gmail.com>
2022-09-13 13:30:47 +0200

    repository stuffs created

---
+++ App/Repository/IRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+interface IRepository
+{
+    public function add();
+
+    public function remove();
+
+    public function find();
+
+    public function update();
+}
---
+++ App/Repository/repository.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'IRepository.php';
+
+class Repository implements IRepository {
+
+    public $QuelqueChose;
+    public $DeuximeQuelqueChose;
+
+    public function add()
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function remove()
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function find()
+    {
+        // TODO: Implement find() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+}
