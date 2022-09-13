179627705353119ba263ab9fcd8729929ef1d499
Ryww0 <laurent.chaitas@gmail.com>
2022-09-13 13:53:09 +0200

    index.php :: use the router

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once 'config/config.php';
 require_once 'Autoloader.php';
 
 use App\Autoloader;
+use App\Service\Router;
+use App\Controller\HomeController; // not created yet
 
 Autoloader::$folderList =
     [
@@ -21,73 +23,15 @@ try {
 
     $router = new Router($_GET['url']);
 
-
-    // HomePage
-    $router->get('/', function () {
-        echo (new HomeController())->invoke();
+    $router->get('/', function (){
+        (new HomeController)->invoke();
     });
 
-    if (Auth::isAuthenticated()) {
-        // Search
-        $router->get('/search', function () {
-            echo (new SearchController())->invoke();
-        });
-    } else {
-        Redirect::to('user/register', true);
-    }
-
-
-    // Sport CRUD methods
-    $router->get('/admin/sport/', function () {
-        echo (new SportController())->invoke();
-    });
-
-    $router->get('/admin/sport/create', function () {
-        echo (new SportController())->addSport();
-    });
-
-    $router->post('/admin/sport/add', function () {
-        (new SportController())->addSport();
-    });
-
-    $router->post('/admin/sport/delete/:id', function ($params) {
-        (new SportController())->deleteSportById($params);
-    });
-
-    $router->post('/admin/sport/update/:id', function ($params) {
-        (new SportController())->updateSportById($params);
-    });
-
-    $router->get('/admin/sport/:id', function ($params) {
-        echo (new SportController())->getSportById($params);
-    });
-
-    // User CRUD methods
-    $router->get('/admin/user/', function () {
-        echo (new UserController())->invoke();
-    });
-
-    $router->get('/admin/user/create', function () {
-        echo (new UserController())->addUser();
-    });
-
-    $router->post('/admin/user/add', function () {
-        (new UserController())->addUser();
-    });
-
-    $router->post('/admin/user/delete/:id', function ($params) {
-        (new UserController())->deleteUserById($params);
-    });
-
-    $router->post('/admin/user/update/:id', function ($params) {
-        (new UserController())->updateUserById($params);
-    });
-
-    $router->get('/admin/user/:id', function ($params) {
-        echo (new UserController())->getUserById($params);
+    $router->post('/add', function (){
+        (new HomeController)->add();
     });
 
     $router->run();
-} catch (RouterException|Exception $e) {
+} catch (Exception $e) {
     die('Error: ' . $e);
 }
