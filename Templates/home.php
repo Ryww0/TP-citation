3afc6797b4dd6fd3a4c68edb6b74a2ec12fa8be3
Fr3dplan <fr3d.web@gmail.com>
2022-09-13 14:37:26 +0200

    view ok

---
+++ Templates/home.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="row">
+                <?php foreach ($citations as $citation) { ?>
+                    <div class="col-3">
+                        <h1><?= $citation->getCitation() ?></h1>
+                        <h3>-- <?php echo $citation->getAuteur() ?> </h3>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
