54e2ff7d17248a37a58fe49b9ab52666633c6938
kava <wabutrune.p@hotmail.com>
2022-09-13 14:28:42 +0200

    formication 0.1

--- App/Form/Formcitation.php
+++ App/Form/Formcitation.php
@@ -1,2 +1,23 @@
 <?php 
 include './App/Service/form.php';
+
+$form = new Form;
+
+$form->debutForm()
+    ->ajoutLabelFor('email', 'Email')
+    ->ajoutInput('email', 'email', ['id' => 'email', 'class' => 'form-control'])
+    ->ajoutLabelFor('password', 'Mot de passe')
+    ->ajoutInput('password', 'password', ['id' => 'password', 'class' => 'form-control'])
+    ->ajoutBouton('Me connecter', ['class' => 'btn btn-primary'])
+    ->finForm();
+
+$loginForm = $form->create();
+echo $loginForm;
+
+if (Form::validate($_POST, ['titre', 'description'])) {
+    var_dump("ajout d'annonce");
+}
+
+if(Form::validate($_POST, ['titre', 'description'])){
+    var_dump("modification d'annonce");
+}
