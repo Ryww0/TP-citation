b45d344f2c15816174a34a0138ceb41f59fb320b
stomruk <s.tomruk35@gmail.com>
2022-09-13 13:57:11 +0200

    Repository v0.25

--- App/Repository/repository.php
+++ App/Repository/repository.php
@@ -6,23 +6,40 @@ class Repository implements IRepository {
     public $QuelqueChose;
     public $DeuximeQuelqueChose;
 
+    public function __construct($value1, $value2) {
+        $this->QuelqueChose = $value1;
+        $this->DeuximeQuelqueChose = $value2;
+    }
+
     public function add()
     {
-        // TODO: Implement remove() method.
+        $request = $this->connection->prepare("INSERT INTO TableName (1erValue, 2emeValue) VALUES (:value1,:value2)");
+        $request->execute(array(
+            'valuer1' => $this->QuelqueChose,
+            'valuer2' => $this->DeuximeQuelqueChose
+        ));
     }
 
     public function remove()
     {
-        // TODO: Implement remove() method.
+        $request = $this->connection->prepare("DELETE FROM TableName WHERE quelquechose = :quelquechose");
+        $request->bindParam(':quelquechose', $this->QuelqueChose);
+        $request->execute();
     }
 
     public function find()
     {
-        // TODO: Implement find() method.
+        $request = $this->connection->prepare("SELECT FROM TableName WHERE quelquechose=:quelquechose");
+        $request->bindParam('quelquechose', $this->QuelqueChose);
+        $request->execute();
     }
 
     public function update()
     {
-        // TODO: Implement update() method.
+        $request = $this->connection->prepare("UPDATE TableName SET quelquechose = :quelquechose WHERE quelquechoseID = :quelquechoseID");
+        $request->execute(array(
+            'quelquechose' => $this->QuelqueChose,
+            'quelquechoseID' => $this->DeuximeQuelqueChose
+        ));
     }
 }
