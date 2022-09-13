9e82ea2535c7eefe6b7970938b5bb2faa33a05e6
Ryww0 <laurent.chaitas@gmail.com>
2022-09-13 14:15:34 +0200

    config.php :: initialized && done

---
+++ Config/config.php
@@ -0,0 +1,14 @@
+<?php
+//nom du site
+const SITE_NAME = 'Rencontres sportives';
+
+//App Root
+define("APP_ROOT", dirname(__FILE__, 2));
+const URL_ROOT = '/ConcretExample/';
+const URL_SUBFOLDER = '';
+
+//DB Params
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'test';
