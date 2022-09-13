dcf31aae2ce1071730cf3da1e9f16b097d429a77
kava <wabutrune.p@hotmail.com>
2022-09-13 13:57:46 +0200

    BDD 0.2

--- BDD/BDD.sql
+++ BDD/BDD.sql
@@ -1,12 +1,9 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+DROP DATABASE IF NOT EXISTS `TP_citation`;
+
 CREATE DATABASE IF NOT EXISTS `TP_citation` CHARACTER SET 'utf8';
+
 USE `TP_citation`;
 
-#------------------------------------------------------------
-# Table: citations
-#------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `citations`(
         `id`        INT (11) AUTO_INCREMENT  NOT NULL ,
@@ -15,11 +12,32 @@ CREATE TABLE IF NOT EXISTS `citations`(
         PRIMARY KEY (`id`)
 )ENGINE=InnoDB;
 
+INSERT INTO
+    Citations (
+        id,
+        auteur,
+        contenu
+    );
 
+VALUES (
+        '1',
+        'Jules Ceasar',
+        'J\'ai la Gaulle'
+    ), (
+        '2',
+        'Dark Jador',
+        'J\'essuie ton père'
+    ), (
+        '3',
+        'Napoleon',
+        'Satanés Anglais !'
+    ), (
+        '4',
+        'Fred',
+        'Le PHP c\'est génial'
+    ), (
+        '5',
+        'Karadoc',
+        'Le gras, c\'est la vie !'
+    );
 
--- // ALTER TABLE appointments 
--- // ADD CONSTRAINT FK_appointments_id_patients
--- //   FOREIGN KEY (idPatients)
--- //   REFERENCES patients (id)
--- //   ON DELETE CASCADE
--- //   ON UPDATE NO ACTION;
