d0432b311bd08e7166f9c8107f2cfe00b9d0ae7d
stomruk <s.tomruk35@gmail.com>
2022-09-13 14:26:56 +0200

    config created

---
+++ Config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//nom du site
+define('SITE_NAME', 'CRY CAT');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+définir ('URL_ROOT', '/');
+définir('URL_SUBFOLDER', '');
+
+//DB Params
+définir ('DB_HOST', 'phpMyAdmin');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Citation');
